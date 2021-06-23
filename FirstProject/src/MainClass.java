d80a6abe40690b87a5868bce58c02edb87cf0c69
sujinkim123 <alicekim0429@naver.com>
2021-06-24 00:46:18 +0900

    FirstProject

---
+++ FirstProject/src/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+	public static void main(String[] args) {	// main 입력 후 ctrl + spacebar 후 enter
+		// 출력 (주석)
+		System.out.println("Hello Java!");
+	}
+}
+
---
+++ FirstProject/src/MainClass2.java
@@ -0,0 +1,7 @@
+
+public class MainClass2 {
+	public static void main(String[] args) {
+		// Hello 출력
+		System.out.println("Hello");
+	}
+}
